70208793fd68ccaf09af9e2a4011199aca98ea57
David Kucher <mrdkucher@users.noreply.github.com>
2017-06-21 09:17:00 -0500

    Added comments

--- Reddit_Scraper_Complete/redditscrape.py
+++ Reddit_Scraper_Complete/redditscrape.py
@@ -2,7 +2,6 @@ import bz2
 import urllib.request
 import os
 import csv
-import sys
 import json
 
 # define user_agent so http request is not denied
@@ -52,19 +51,26 @@ def parsejson(infile, outfile, subreddit, fields):
 # repeat over how many years user desires
 for year in range(int(startyear), int(endyear) + 1):
     for month in months:
+        # create file name from year according to download format from https://files.pushshift.io
         string = "RS_" + str(year) + "-" + month + ".bz2"
+        # create output file in format year-month.csv
         csvfile = str(year) + "-" + month + ".csv"
+        # create url to download file
         file_url = "http://files.pushshift.io/reddit/submissions/" + string
-
-        request = urllib.request.Request(file_url,None,headers) #The assembled request
-        response = urllib.request.urlopen(request)
-        try:
-            data = bz2.decompress(response.read())
         
+        # request the file
+        request = urllib.request.Request(file_url,None,headers)
+        response = urllib.request.urlopen(request)
+        # try to decompress and write the data into a temp file, if it exists(some data from 2006 and 2007 is missing)
+        try:
+            # decompress data
+            data = bz2.decompress(response.read())
+            # write into "temp" file which is overwritten for each month to save memory
             with open("temp", "wb") as code:
                 code.write(data)
-
+            # call parsejson function to go through the decompressed file and select the desired fields from the desired subreddit
             parsejson("temp", csvfile, subreddit, fields)
+        # if data is missing, print year and month of missing data
         except:
             print("Reddit data for the year: "+ str(year) + " and month: " + month + " is missing.")
 
