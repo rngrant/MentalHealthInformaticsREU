28f2d89841bbcb55bc60fef1acfcf93cdf289732
reilly <reilly.grant@gmail.com>
2017-06-25 10:50:46 -0500

    updated model creation

--- word2vecAnalysis/createModel.py
+++ word2vecAnalysis/createModel.py
@@ -5,5 +5,5 @@ import dataUtils
 sentences = dataUtils.read('data',"selftext")
 
 # construct and save a basic model
-model = gensim.models.Word2Vec([dataUtils.cleanSentence(s).split() for s in sentences])
+model = gensim.models.Word2Vec([dataUtils.cleanSentence(s).split() for s in sentences],min_count =10, sg=1, size =100)
 model.save('models/basicModel.model')
--- word2vecAnalysis/dataUtils.py
+++ word2vecAnalysis/dataUtils.py
@@ -1,15 +1,20 @@
 import gensim, logging, os, csv
 
-def read(dirname,str):
+# Helper function for reading from a directory of CSVs
+# dirname: the name of the directory of CSVs
+# key: The name of the variable to extract from the CSV
+# returns a generator that returns the next string in the file
+def read(dirname,key):
         for fname in os.listdir(dirname):
             first = True
             loc =0
-            with open(dirname+ "/"+fname,'r') as csvfile:
+            with open(dirname+ "/"+fname,'r') as csvfile:                
                 reader = csv.reader(csvfile,dialect='excel',delimiter=',',quotechar='\"')
                 try:
                     for row in reader :
+                        # get the location the variable to extract
                         if first:
-                            loc = row.index(str)
+                            loc = row.index(key)
                             first = False
                         else:
                             yield row[loc]                            
@@ -17,10 +22,14 @@ def read(dirname,str):
                     print (fname + " has an error")
                 csvfile.close()
 
+# Takes a string and returns a cleaned version                
 def cleanSentence(sentence):
+    # remove deleted sentences
     if sentence == "[deleted]":
         return ""
     else:
+        # remove case
         sentence = sentence.lower()
-        exclude = ",.;:)(123456789"
+        # remove special characters
+        exclude = ",.;:)([]0123456789/?*$\"!~\n^"
         return ''.join(ch for ch in sentence if ch not in exclude)
