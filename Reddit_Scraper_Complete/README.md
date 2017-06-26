b597cbbf483e21f266bc4a3981e9561430a370b8
reilly <reilly.grant@gmail.com>
2017-06-26 09:17:18 -0500

    updated readme

--- Reddit_Scraper_Complete/README.md
+++ Reddit_Scraper_Complete/README.md
@@ -3,9 +3,11 @@
 The files in this directory are used to aquire
 and parse past reddit data. The data is aquired from
 http://files.pushshift.io/ in line delimited JSON format,
-and is parsed to a CSV containing selected fields from a
-specific subreddit. This program returns entire years of data, 
-from which one can select the data one needs.
+and is parsed to a CSV containing selected fields from specific
+subreddits. The fields should be specified in 'fields.csv', and 
+subreddits should be specified in 'subreddits.csv'.
+This program returns entire years of data, from 
+which one can further filter the data one needs if neccessary.
 
 To run this program, one needs at least 20 GB of RAM, as the
 decompressed files reach up to 18 GB in size. One can do this
--- Reddit_Scraper_Complete/redditscrape.py
+++ Reddit_Scraper_Complete/redditscrape.py
@@ -11,9 +11,18 @@ headers={'User-Agent':user_agent}
 months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 
 # prompt user for information
-subreddit = input("Subreddit: ")
 startyear = input("Start year: ")
 endyear = input("End year: ")
+
+subreddits =[]
+try:
+    subreddits_file = open('subreddits.csv', 'r')
+except:
+    print('Error: Could not open subreddits.csv')
+    exit(1)
+subreddit_reader = csv.reader(subreddits_file,delimiter=',', quotechar='\"')
+for subreddits_list in subreddit_reader:
+    subreddits = subreddits_list
 
 # open 'fields.csv' and read as a csv, otherwise return an error
 try:
@@ -23,48 +32,53 @@ except:
     exit(1)
 
 # if fields.csv exists, enter
-reader = csv.reader(fields_file, delimiter=',', quotechar='\"')
-for words in reader:
+fields_reader = csv.reader(fields_file, delimiter=',', quotechar='\"')
+for words in fields_reader:
     fields = words
 
 # define a function that will read a JSONLines file with fields from fields.txt
-def parsejson(infile, outfile, subreddit, fields):
-	# open csv output file, named outfile
-	csvfile = open(outfile, 'w')
-    writer = csv.writer(csvfile, delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
-
+def parsejson(infile,suffix, subreddits, fields):
+	# open csv output files, with the given suffix
+    csvfiles = []
+    writers  = []
+    for i in range(len(subreddits)):
+        csvfile = open(subreddits[i]+"_"+suffix, 'w')
+        writer = csv.writer(csvfile, delimiter=',', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
+        csvfiles.append(csvfile)
+        writers.append(writer)
     # check to see if json has key, if not, return empty string
-	def getValue(key, submission):
-    	if key in submission:
-        	return submission[key]
+    def getValue(key, submission):
+        if key in submission:
+            return submission[key]
         return ""
 
 	# open jsonlines file, iterate through each line, which is a json object,
     with open(infile) as f:
     	# write a row of headings
-        writer.writerow(fields)
+        for writer in writers:            
+            writer.writerow(fields)
         for line in f:
         	# try to load an object from line, otherwise print error and move on
-        	try:
-        		# load object in current line as submission
-			submission = (json.loads(line))
-			# if object is of desired subreddit, then load its contents into csv
-            		if getValue('subreddit', submission) == subreddit:
-                		writer.writerow(list(map(lambda field: getValue(field,submission),fields)))
-    		except:
-    			print('Could not read a line, moving to next one')
+                # load object in current line as submission
+            submission = (json.loads(line))
+                # if object is of desired subreddit, then load its contents into csv
+            for i in range(len(subreddits)):
+                if getValue('subreddit', submission) == subreddits[i]:
+                    writers[i].writerow(list(map(lambda field: getValue(field,submission),fields)))
     # close files
-    csvfile.close()
+    for csvfile in csvfiles:
+        csvfile.close()
+
 
 # repeat over how many years user desires
 for year in range(int(startyear), int(endyear) + 1):
     for month in months:
         # create file name from year according to download format from https://files.pushshift.io
-        string = "RS_" + str(year) + "-" + month + ".bz2"
+        file_name = "RS_" + str(year) + "-" + month + ".bz2"
         # create output file in format year-month.csv
         csvfile = str(year) + "-" + month + ".csv"
         # create url to download file
-        file_url = "http://files.pushshift.io/reddit/submissions/" + string
+        file_url = "http://files.pushshift.io/reddit/submissions/" + file_name
         
         # request the file
         request = urllib.request.Request(file_url,None,headers)
@@ -77,7 +91,7 @@ for year in range(int(startyear), int(endyear) + 1):
             with open("temp", "wb") as code:
                 code.write(data)
             # call parsejson function to go through the decompressed file and select the desired fields from the desired subreddit
-            parsejson("temp", csvfile, subreddit, fields)
+            parsejson("temp", csvfile, subreddits, fields)
         # if data is missing, print year and month of missing data
         except:
             print("Reddit data for the year: "+ str(year) + " and month: " + month + " is missing.")
